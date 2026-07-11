1e7ce8c759b1f9d8d22c141c961c78f9286a0913
v.voloshin <viktor_the_great@mail.ru>
2026-07-11 12:05:22 +0300

    fix: fix update-doc-link-in-readme.sh

--- .bin/update-doc-link-in-readme.sh
+++ .bin/update-doc-link-in-readme.sh
@@ -3,7 +3,6 @@
 package_name=$(npm pkg get name --workspaces=false | tr -d '"')
 doc_version=$(npm pkg get version --workspaces=false | tr -d '"')
 IFS="/" read -r owner repository_name var3 <<< $package_name
-echo "$owner, $repository_name"
 export DOC_LINK="https://${owner}.github.io/${repository_name}/v${doc_version}"
 envsubst < .bin/files/README.md > README.md
 git add README.md
